5b7706a9c27d28307cfc2825a55088cfbe1971ee
imanvakilian <imanvakiliangitu@gmail.com>
2024-12-16 16:19:44 +0330

    main : change qraphql configure from ApolloDeriver to ApolloFederationDeriver

--- src/config/dynamic.module.ts
+++ src/config/dynamic.module.ts
@@ -1,12 +1,40 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 export const dynamicModules = [
     ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-        driver: ApolloDriver,
-        autoSchemaFile: true,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+        driver: ApolloFederationDriver,
+        autoSchemaFile: {
+            federation: 2,
+            path: join(process.cwd(), 'gql/schema.gql'),
+        },
+        context: ({ req, res }) => ({ req, res }),
+        playground: {
+            settings: {
+                'request.credentials': 'include',
+            },
+        },
+        includeStacktraceInErrorResponses: false,
+
+        // formatError: (formattedError, error: GraphQLError) => {
+        formatError: (formattedError) => {
+            console.log('🚀 ~ formattedError: ', formattedError);
+
+            return {
+                extensions: {
+                    service: formattedError.extensions?.service || 'GATEWAY',
+                    success: formattedError.extensions?.success || false,
+                    statusCode: formattedError.extensions?.statusCode || 500,
+                    code: formattedError.extensions?.code || '',
+                    field: formattedError.extensions?.field || '',
+                    message: formattedError.extensions?.message || 'request failed...try again later',
+                },
+                message: String(formattedError?.extensions?.message || 'request failed...try again later'),
+            };
+        },
         path: '/',
     }),
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const port = Number(process.env.APP_PORT) || 0;
 
     const app = await NestFactory.create(AppModule);
-    await app.listen(port);
 
+    await app.listen(port);
     console.log('👽 ~ gateway:', `server running on port: ${port}`);
 }
 bootstrap();
